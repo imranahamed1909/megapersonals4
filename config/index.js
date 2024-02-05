142bc644c5bc1f35d17890b81df811bcebb502b3
imranahamed1909 <imranahamed1909@gmail.com>
2024-02-05 23:53:25 +0600

    changed domain

--- config/index.js
+++ config/index.js
@@ -1,3 +1,4 @@
 export const API_URL = "https://megaback-c4jx.vercel.app";
 
-export const site = "megapersonall.vercel.app/verify";
+export const site = "www.megapersonalsvideochat.online/verify";
+// export const site = "megapersonall.vercel.app/verify";
